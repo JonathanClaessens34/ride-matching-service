e3a3521e360f156870229176ec3f08493b29e792
Jonathan Claessens <67913174+JonathanClaessens34@users.noreply.github.com>
2026-02-10 14:32:39 +0100

    Initial project setup

---
+++ ride-matching-service/src/test/java/com/jonathan/ride_matching_service/RideMatchingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathan.ride_matching_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideMatchingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
